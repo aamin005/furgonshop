fe85838112cecb94df51ae4fd3f6307553534f1a
OEM Configuration (temporary user) <aamin005@gmal.com>
2023-06-18 13:40:08 +0700

    Product Details Screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,7 +14,6 @@ function Header() {
             <Nav className="me-auto">
               <LinkContainer to="/cart"><Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link></LinkContainer>
               <LinkContainer to="/login"><Nav.Link><i className="fas fa-user"></i> Login</Nav.Link></LinkContainer>
-
             </Nav>
           </Navbar.Collapse>
         </Container>
