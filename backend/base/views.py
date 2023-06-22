48a0d1ec25e2889a745ffdc27fbfdbe000d8d82e
OEM Configuration (temporary user) <aamin005@gmal.com>
2023-06-22 21:19:02 +0700

    JWT customization

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,10 +2,29 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from typing import Dict, Any
+
 
 from .products import products
 from .models import Product
-from .serializer import ProductSerializer
+from .serializer import ProductSerializer, UserSerializer
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs: Dict[str, Any]) -> Dict[str, str]:
+        data = super().validate(attrs)
+        
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -25,6 +44,14 @@ def getRoutes(request):
 
     ]
     return Response(routes)
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getProducts(request):
